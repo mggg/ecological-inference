9553b9b15b6df37b6ebf27d8d1362097d40672c3
Colin Carroll <colcarroll@gmail.com>
2020-08-02 16:57:09 -0400

    Add data utility, update demo

---
+++ pyei/data.py
@@ -0,0 +1,44 @@
+"""Helpers for managing data files."""
+import io
+import os
+import pkgutil
+
+import pandas as pd
+
+__all__ = ["get_data"]
+
+
+def get_data(filename):
+    """Returns a dataframe for relevant datasets.
+
+    Will either load remotely, or locally. Currently supports:
+
+    - santaClara.csv
+    - waterbury.csv
+
+    TODO: Add details about these datasets.
+
+    Parameters
+    ----------
+    filename: str
+        File to load. See above for valid files.
+
+    Returns
+    -------
+    BytesIO of the data
+    """
+    if filename == "santaClara.csv":
+        return pd.read_csv(
+            "https://raw.githubusercontent.com/gerrymandr/ei-app/master/santaClara.csv"
+        )
+    elif filename == "waterbury.csv":
+        return pd.read_csv(
+            "https://raw.githubusercontent.com/gerrymandr/ei-app/master/santaClara.csv"
+        )
+    # This does not work yet (9/2/20), but will collect
+    # files checked into pyei/examples/data/<filename>
+    else:
+        data_pkg = "pyei.examples"
+        return pd.read_csv(
+            io.BytesIO(pkgutil.get_data(data_pkg, os.path.join("data", filename)))
+        )
