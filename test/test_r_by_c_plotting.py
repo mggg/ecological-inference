8446c9181662543a71efaee0d5d9a6612888e2cd
Karin Knudson <karink520@gmail.com>
2021-02-18 08:50:00 -0500

    test linting

--- test/test_r_by_c_plotting.py
+++ test/test_r_by_c_plotting.py
@@ -1,12 +1,12 @@
-from pyei import data
+"""test r-by-c specific plotting"""
 import pytest
 import numpy as np
+from pyei import data
 from pyei.r_by_c import RowByColumnEI
-
 
 @pytest.fixture(scope="session")
 def example_r_by_c_data():
-    """"""
+    """trimmed santa clara dataset"""
     sc_data = data.Datasets.Santa_Clara.to_dataframe()
     sc_data = sc_data.iloc[:10, :]
     precinct_pops = np.array(sc_data["total2"])
@@ -27,16 +27,16 @@ def example_r_by_c_data():
 
 @pytest.fixture(scope="session")
 def example_r_by_c_ei(example_r_by_c_data):
-    """"""
-    ei = RowByColumnEI(model_name="multinomial-dirichlet")
-    ei.fit(
+    """run an r by c ei example method"""
+    ei_ex = RowByColumnEI(model_name="multinomial-dirichlet")
+    ei_ex.fit(
         example_r_by_c_data["group_fractions"],
         example_r_by_c_data["votes_fractions"],
         example_r_by_c_data["precinct_pops"],
         example_r_by_c_data["demographic_group_names"],
         example_r_by_c_data["candidate_names"],
     )
-    return ei
+    return ei_ex
 
 
 def test_ei_r_by_c_boxplots(example_r_by_c_ei):
