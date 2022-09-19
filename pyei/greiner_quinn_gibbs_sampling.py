82009b4bf68bb5f3d3555243021a2819b170424c
Chanel Richardson <crichardson1399@gmail.com>
2022-09-19 14:53:50 -0400

    gq

--- pyei/greiner_quinn_gibbs_sampling.py
+++ pyei/greiner_quinn_gibbs_sampling.py
@@ -468,12 +468,9 @@ def sample_internal_cell_counts(theta_samp, prev_internal_counts_samp):
                         pi1 = theta_samp[i, r, c]
                         pi2 = theta_samp[i, r_prime, c]
                         psi = (pi1 * (1 - pi2)) / (pi2 * (1 - pi1))
-                        try:
-                            r_c_count = non_central_hypergeometric_sample(
-       	                        n1, n2, m1, psi
-                	        )  # sample for the r, c internal count
-                        except:
-                            print("hyper geom")
+                        r_c_count = non_central_hypergeometric_sample(
+                             n1, n2, m1, psi
+               	        )  # sample for the r, c internal count
                         # update prev_internal counts in the 2 x 2 subarray
                         prev_internal_counts_samp[i, r, c] = r_c_count
                         prev_internal_counts_samp[i, r, c_prime] = n1 - r_c_count
